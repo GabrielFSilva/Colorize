75aa53285b4f594650ba2be365884139add0f20b
Gabriel Fonseca Silva <gabrielsilva.prog@gmail.com>
2014-08-18 01:15:12 -0300

    Project Setup

---
+++ Assets/Scripts/GFSCustomButton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System;
+
+public class GFSCustomButton : MonoBehaviour 
+{
+	public event Action<string> onClick;
+	public event Action<string> onDrop;
+	public event Action<bool,string> onPress;
+
+	void OnClick()
+	{
+		if(onClick != null)
+		{
+			onClick(gameObject.name);
+		}
+	}
+
+	void OnDrop()
+	{
+		if(onDrop != null)
+		{
+			onDrop(gameObject.name);
+		}
+	}
+
+	void OnPress(bool p_pressed)
+	{
+		if(onPress != null)
+		{
+			onPress(p_pressed);
+		}
+
+	}
+}
